df6e64998a2adebc207a3101f60fac1ea8f045cc
kfc0824 <2578198711@qq.com>
2017-07-29 12:15:34 +0800

    add test for big or little endian

---
+++ learning/Fc2-big_little_endian.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    short s = 0x1234;
+    char a0 = (char)((char*)(&s))[0];
+    char a1 = (char)((char*)(&s))[1];
+
+    printf("value: 0x%x, a0: 0x%x, a1: 0x%x\n", s, a0, a1);
+    if(a0 == 0x34)
+    {
+        printf("little endian\n");
+    }
+    else
+    {
+        printf("big endian\n");
+    }
+
+    return 0;
+}
